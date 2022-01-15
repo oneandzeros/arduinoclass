36d07530b572d926f872171ae08620336d4a68ab
oneandzeros <tobinok@gmail.com>
2022-01-15 09:53:12 +0800

    try first upload

---
+++ sketch_if.ino
@@ -0,0 +1,44 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  //if template 1
+  if () {
+    //code 1
+  }
+  
+  //if template 2
+  if () {
+    //code 1
+  } else {
+    //code 2
+  } 
+
+  //if template 3
+  int a;
+  //
+  a = random(10000);
+  //
+  if (a > 0) {
+  } else if (a > 100) {
+  } else if (a > 1000) {
+  } else if (a > 5000)  {
+  }
+
+
+  switch() {
+  }
+
+
+  int i =0
+  while(i < 100) {
+    Serial.println(i);
+  }
+
+
+  
+}
